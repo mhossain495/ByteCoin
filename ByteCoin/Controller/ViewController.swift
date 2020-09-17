20725f3f6cd045cc8a8c4aa37f561157d23d4272
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-09-16 23:46:26 -0400

    Add UIPickerView to allow selection of currencies

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -1,18 +1,49 @@
 //
 //  ViewController.swift
 //  ByteCoin
-//
-//  Created by Angela Yu on 11/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    let coinManager = CoinManager()
+    
+    // Number of columns in picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    // Number of rows or items in picker based on array count
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    // Set values of picker equal to values in array
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    // Called when user selects a row in user interface
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(coinManager.currencyArray[row])
+    }
 
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Set current ViewController as dataSource and delegate
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
     }
 
 
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -1,10 +1,8 @@
 //
 //  CoinManager.swift
 //  ByteCoin
-//
-//  Created by Angela Yu on 11/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+
+
 
 import Foundation
 
@@ -15,5 +13,8 @@ struct CoinManager {
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    func getCoinPrice(for currency: String) {
+        <#function body#>
+    }
     
 }
