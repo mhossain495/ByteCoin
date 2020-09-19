bc7e887278e02c75a84e44d3c2a8a099df6b9298
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-09-18 23:58:52 -0400

    Add CoinData structure file and parseJSON function to retrieve last price

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -6,7 +6,13 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC/"
     let apiKey = "44120C57-C21D-45C7-99EE-8A316EF8AE53"
@@ -25,26 +31,43 @@ struct CoinManager {
             // 2. Create a URLSession
             let session = URLSession(configuration: .default)
             
-             //3, Give the session a task
+            //3, Give the session a task
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
-                    //self.delegate?.didFailWithError(error: error!)
-                    //return // exits function if error with url
+                    self.delegate?.didFailWithError(error: error!)
+                    return // exits function if error with url
                 }
                 if let safeData = data {
-                    //if let price = self.parseJSON(safeData) {
+                    if let lastPrice = self.parseJSON(safeData) {
                     let dataString = String(data: safeData, encoding: .utf8)
-                        //self.delegate?didGetPrice(self, price: price)
+                    //self.delegate?didGetPrice(self, price: price)
                     print(dataString!)
+                    print(lastPrice)
                 }
-                    
-                    
-                }
+                
+                
+            }
+            }
             
-             //4. Start the task
+            //4. Start the task
             task.resume()
         }
         
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            return lastPrice
+            
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
 
 }
-}
+
